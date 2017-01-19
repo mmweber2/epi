fe036f8e7aaa6c71bbea1d0b6529343f996e53de
Mary Ellen Weber <mmweber2@gmail.com>
2017-01-19 10:50:28 -0600

    Begin updating 5.2 per code review.

--- java/Solution5_2.java
+++ java/Solution5_2.java
@@ -1,13 +1,15 @@
 public class Solution5_2 {
+
     public static long swapBits(long x, int i, int j) {
-        if ((x >>> i) & 1) != ((x >>> j) & 1) {
+        if (((x >>> i) & 1) != ((x >>> j) & 1)) {
             return x ^ ((1L << i) | (1L << j));
+        }
         return x;
-  }
+    }
 
 }
 
 // Note: In Python, I compared the two set bits by doing (1 << i) & x.
 // This also works in Java, but it's possible that it would shift out
-// of the range of an int, so the right shifting looks safer.
+// of the range of an int, so the right shifting is safer.
 // However, it might work just as well if I used 1L instead of 1.
